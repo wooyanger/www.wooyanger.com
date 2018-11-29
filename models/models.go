70de29c9e8239177336d4fe3927e7d6a6506fcd3
mike <819844849@qq.com>
2018-11-29 18:53:12 +0800

    优化读取配置文件功能

--- models/models.go
+++ models/models.go
@@ -1,1 +1,22 @@
 package models
+
+import (
+	"github.com/go-xorm/xorm"
+	"www.wooyanger.com/pkg/logs"
+	"www.wooyanger.com/pkg/setting"
+)
+
+var (
+	x	*xorm.Engine
+	e	error
+)
+
+func init()  {
+	x, e = xorm.NewEngine("mysql", setting.DbDsn)
+	if e != nil {
+		logs.Fatalf("%v", e)
+	}
+	if e = x.Ping();e != nil {
+		logs.Fatalf("%v", e)
+	}
+}
--- pkg/setting/setting.go
+++ pkg/setting/setting.go
@@ -10,6 +10,13 @@ var (
 	HttpHost	string
 	HttpPort	int64
 
+	DbHost		string
+	DbPort		int64
+	DbUser		string
+	DbPass		string
+	DbName		string
+
+	DbDsn		string
 	ListenAddr	string
 	Cfg			*ini.File
 	Error		error
@@ -21,7 +28,14 @@ func init() {
 		logs.Fatalf("%v", Error)
 	}
 	ServerSec := Cfg.Section("server")
+	DatabaseSec := Cfg.Section("database")
 	HttpHost = ServerSec.Key("HOST").MustString("127.0.0.1")
 	HttpPort = ServerSec.Key("PORT").MustInt64(1110)
 	ListenAddr = fmt.Sprintf("%s:%d", HttpHost, HttpPort)
+	DbHost = DatabaseSec.Key("HOST").MustString("127.0.0.1")
+	DbPort = DatabaseSec.Key("Port").MustInt64(3306)
+	DbUser = DatabaseSec.Key("USER").MustString("root")
+	DbPass = DatabaseSec.Key("PASS").MustString("root")
+	DbName = DatabaseSec.Key("NAME").MustString("www.wooyanger.com")
+	DbDsn = fmt.Sprintf("%s:%s@tcp(%s:%d)/%s?charset=utf8", DbUser, DbPass, DbHost, DbPort, DbName)
 }
