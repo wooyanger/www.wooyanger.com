58b067e820ab24bd3571b163f7ef4ea26b700526
mike <819844849@qq.com>
2018-11-30 14:18:03 +0800

    增加图片上传功能

---
+++ controllers/file.go
@@ -0,0 +1,22 @@
+package controllers
+
+import (
+	"www.wooyanger.com/pkg/tools"
+)
+
+const SuccessMsg  = "file upload successful."
+
+type FileController struct {
+	Controllers
+}
+
+func (f *FileController) PostUpload() {
+	file, fileheader, err := f.Ctx.FormFile("file")
+	if err != nil {
+		f.Ctx.JSON(map[string]interface{}{"code": 1, "state": "fatal", "msg": err})
+	}
+	if err = tools.CreateImgFile(file, *fileheader); err != nil {
+		f.Ctx.JSON(map[string]interface{}{"code": 2, "state": "fatal", "msg": err})
+	}
+	f.Ctx.JSON(map[string]interface{}{"code": 0, "state": "success", "msg": SuccessMsg,})
+}
---
+++ pkg/tools/file.go
@@ -0,0 +1,53 @@
+package tools
+
+import (
+	"fmt"
+	"github.com/satori/go.uuid"
+	"io"
+	"mime/multipart"
+	"net/http"
+	"os"
+	"strings"
+)
+
+func IsFileExist(filename string) bool {
+	if _, err := os.Stat(filename); os.IsExist(err) {
+		return true
+	}
+	return false
+}
+
+func IsImageFile(data []byte) bool {
+	return strings.Contains(http.DetectContentType(data),"image/")
+}
+
+func IsPngImageFile(data []byte) bool {
+	return strings.Contains(http.DetectContentType(data), "image/png")
+}
+
+func IsJpegImageFile(data []byte) bool {
+	return strings.Contains(http.DetectContentType(data), "image/jpeg")
+}
+
+func IsGifImageFile(data []byte) bool {
+	return strings.Contains(http.DetectContentType(data), "image/gif")
+}
+
+func FileExtension(fileheader multipart.FileHeader) string {
+	filename := fileheader.Filename
+	s := strings.Split(filename, ",")
+	return s[len(s)-1]
+}
+
+func CreateImgFile(file multipart.File, fileheader multipart.FileHeader) error {
+	newFilename := fmt.Sprintf("%s.%s", uuid.Must(uuid.NewV4()), FileExtension(fileheader))
+	out, err := os.OpenFile(newFilename, os.O_WRONLY|os.O_CREATE, 0600)
+	if err != nil {
+		return err
+	}
+	defer out.Close()
+	if _, err = io.Copy(out, file); err != nil {
+		return err
+	}
+	return nil
+}
