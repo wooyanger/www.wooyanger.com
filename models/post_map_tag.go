b371ed6d7f3d5895a3fcad28b05ae0643ee552ab
mike <819844849@qq.com>
2018-12-05 19:48:23 +0800

    增加获取指定标签下所有文章

--- models/post_map_tag.go
+++ models/post_map_tag.go
@@ -5,3 +5,9 @@ type PostMapTag struct {
 	Pid			int64		`xorm: "int(12) not null"`
 	Tid			int64		`xorm: "int(12) not null"`
 }
+
+func GetAllPidByTid(id int64) []PostMapTag {
+	allPostMapTag := make([]PostMapTag, 0)
+	x.Find(&allPostMapTag)
+	return allPostMapTag
+}
