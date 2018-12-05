5b480b86c59878cd2a3e46edf13ba4033e8ba03d
mike <819844849@qq.com>
2018-12-05 19:45:33 +0800

    增加创建文章页面

--- controllers/post.go
+++ controllers/post.go
@@ -1,18 +1,49 @@
 package controllers
 
-import "github.com/kataras/iris/mvc"
+import (
+	"github.com/kataras/iris/mvc"
+	"www.wooyanger.com/models"
+)
 
 type PostController struct {
 	Controllers
 }
 
-func (c *PostController) GetBy(id int64) mvc.Result {
-	p := c.Post.Get(id)
+func (p *PostController) GetBy(id int64) mvc.Result {
+	post := p.Post.GetPostById(id)
 	return mvc.View{
 		Name: "post/view.html",
 		Data: map[string]interface{}{
 			"Title": "管理后台",
-			"Post": p,
+			"Post": post,
 		},
 	}
+}
+
+func (p *PostController) GetNew() mvc.Result {
+	if p.IsLogged() {
+		return mvc.View{
+			Name: "post/new.html",
+			Data: map[string]interface{}{
+				"Title": "",
+				"Authenticated": true,
+				"RequireQuillPlugin": true,
+			},
+		}
+	} else {
+		return mvc.Response{
+			Path: loginUrl,
+			Code: 302,
+		}
+	}
+}
+
+func (p *PostController) PostNew() {
+	newPost := &models.Post{}
+	newPost.Title = p.Ctx.FormValue("title")
+	newPost.Content = p.Ctx.FormValue("content")
+	if err := models.CreatePost(newPost); err != nil {
+		return
+	}
+
 }
